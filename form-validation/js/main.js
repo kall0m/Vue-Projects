f0035acfb1a588f498f1c14619b50defcf147ab9
Kaloyan Madzhunov <k.madjunov@gmail.com>
2020-01-12 00:11:54 +0100

    Add prevention to submit form if fields are empty or invalid; save username and password to localstorage

--- form-validation/js/main.js
+++ form-validation/js/main.js
@@ -2,14 +2,52 @@ const app = new Vue({
     el: '#app',
     data: {
         errors: [],
-        firstName: null,
-        name: null,
-        email: null,
-        username: null,
-        password: null,
-        confirmPassword: null
+        passwordRules: [
+            { message: 'Contains lowercase', regex: /[a-z]+/ },
+            { message: "Contains uppercase", regex: /[A-Z]+/ },
+            { message: "8 characters minimum", regex: /.{8,}/ },
+            { message: "Contains Number", regex: /[0-9]+/ }
+        ],
+        emailRules: [
+            { message: 'Email not correct', regex: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ },
+            { message: 'Email not PCN email', regex: /student\.fontys\.nl/ }
+        ],
+        firstName: '',
+        name: '',
+        email: '',
+        username: '',
+        password: '',
+        confirmPassword: '',
+        firstNameFocused: false,
+        nameFocused: false,
+        emailFocused: false,
+        usernameFocused: false,
+        passwordFocused: false,
+        confirmPasswordFocused: false
+    },
+    mounted() {
+        if (localStorage.username) {
+            this.username = localStorage.username;
+        }
     },
     methods: {
+        persist() {
+            localStorage.username = this.username;
+            localStorage.password = this.password;
+        },
+        login() {
+            let userValid = localStorage.username == this.username;
+            let passValid = localStorage.password == this.password;
+
+            if (userValid && passValid) {
+                alert('great!');
+                return true;
+            } else {
+                alert('who aere you?!');
+            }
+
+            return false;
+        },
         checkForm: function (e) {
             this.errors = [];
 
@@ -23,8 +61,6 @@ const app = new Vue({
 
             if (!this.email) {
                 this.errors.push('Email required.');
-            } else if (!this.validEmail(this.email) || this.notFontysEmail(this.email)) {
-                this.errors.push('Valid email required.');
             }
 
             if (!this.username) {
@@ -36,25 +72,74 @@ const app = new Vue({
             }
 
             if (!this.confirmPassword) {
-                this.errors.push("Please confirm password.");
+                this.errors.push("Confirm password required.");
             }
 
-            if (this.password !== this.confirmPassword) {
-                this.errors.push("Passwords do not match.");
-            }
+            if (!this.errors.length && this.emailValidation.valid && this.passwordValidation.valid) {
+                this.persist();
 
-            if (!this.errors.length) {
                 return true;
             }
 
             e.preventDefault();
         },
-        validEmail: function (email) {
-            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
+        fieldRegexValidation(rules, field) {
+            let errors = [];
+
+            for (let rule of rules) {
+                if (!rule.regex.test(field)) {
+                    errors.push(rule.message);
+                }
+            }
+
+            if (errors.length === 0) {
+                return { valid: true, errors }
+            } else {
+                return { valid: false, errors }
+            }
+        }
+    },
+    computed: {
+        firstNameMissing() {
+            return this.firstName === '';
         },
-        notFontysEmail(value = "") {
-            return !value.includes("student.fontys.nl");
+        nameMissing() {
+            return this.name === '';
+        },
+        emailMissing() {
+            return this.email === '';
+        },
+        usernameMissing() {
+            return this.username === '';
+        },
+        passwordMissing() {
+            return this.password === '';
+        },
+        confirmPasswordMissing() {
+            return this.confirmPassword === '';
+        },
+        emailValidation() {
+            return this.fieldRegexValidation(this.emailRules, this.email);
+        },
+        notSamePasswords() {
+            if (this.passwordsFilled) {
+                return (this.password !== this.confirmPassword);
+            } else {
+                return false;
+            }
+        },
+        passwordsFilled() {
+            return (this.password !== '' && this.confirmPassword !== '');
+        },
+        passwordValidation() {
+            let result = this.fieldRegexValidation(this.passwordRules, this.password);
+
+            if (this.notSamePasswords) {
+                result.valid = false;
+                result.errors.push("Passwords don't match");
+            }
+
+            return result;
         }
     }
 })
